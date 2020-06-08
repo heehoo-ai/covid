a3472c98ace915cc2b29eab9358898f3502f0e66
heehoo-ai <heehoo@qq.com>
2020-06-08 15:53:04 +0800

    echarts实例

---
+++ spyder/main.py
@@ -0,0 +1,14 @@
+import requests
+
+class YQ():
+    def __init__(self):
+        pass
+
+    def get_data(self):
+        pass
+
+    def save_data(self):
+        pass
+
+    if __name__ == "__main":
+        yq = YQ()
